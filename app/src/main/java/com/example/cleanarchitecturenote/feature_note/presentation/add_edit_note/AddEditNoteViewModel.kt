411b77321d7ba4115336d035eb12cc308ff46b95
unknown <singhrishikesh500@gmail.com>
2024-08-25 23:53:51 +0530

    leanring PR raising system

--- app/src/main/java/com/example/cleanarchitecturenote/feature_note/presentation/add_edit_note/AddEditNoteViewModel.kt
+++ app/src/main/java/com/example/cleanarchitecturenote/feature_note/presentation/add_edit_note/AddEditNoteViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.cleanarchitecturenote.feature_note.presentation.add_edit_note
 
 import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.toArgb
 import androidx.lifecycle.ViewModel
@@ -16,16 +15,21 @@ import javax.inject.Inject
 class AddEditNoteViewModel @Inject constructor(
     private val addNoteUseCase: AddNoteUseCase
 ) : ViewModel() {
-    
+
     private val _noteTitle = mutableStateOf(NoteTextFieldState())
-    val noteTitle : State<NoteTextFieldState> = _noteTitle
+    val noteTitle: State<NoteTextFieldState> = _noteTitle
 
     private val _noteContent = mutableStateOf(NoteTextFieldState())
-    val noteContent : State<NoteTextFieldState> = _noteContent
+    val noteContent: State<NoteTextFieldState> = _noteContent
 
     private val _noteColor = mutableStateOf(Note.noteColors.random().toArgb())
-    val noteColor : State<Int> = _noteColor
+    val noteColor: State<Int> = _noteColor
 
 
-   // private val _eventFlow = MutableSharedFlow<>()
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+
+    sealed class UiEvent {
+        data class ShowSnackBar(val message: String) : UiEvent()
+        data object SaveNote : UiEvent()
+    }
 }
